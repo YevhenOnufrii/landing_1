cd161e4c73b52a7de83fb82fae76f510ec002bbc
Yevhen <onufrii.socnet@gmail.com>
2023-07-26 00:50:48 +0300

    add about component

--- src/App.jsx
+++ src/App.jsx
@@ -103,6 +103,34 @@ function Main() {
   )
 }
 
+function About() {
+  return (
+    <div className="about">
+      <div className="about__title">
+        <h3 className="about__title_text title">
+          Miami best <br /> Real estate <span>here</span>
+        </h3>
+      </div>
+      <div className="about__text">
+        <p className="about__text_header">Over 50 years experience in love.</p>
+        <p className="about__text_main">
+          Mazel and VA Tsukkerman in his &quot;Analysis of musical works&quot;. Pointillism, which
+          originated in the music of the early twentieth century, microforms, found a distant
+          historical. <br />
+          <br />
+          The market leader in real estate since 2003. Awards Real Estate Company, 2010, 2011, 2013.
+          More than 200 satisfied customers premium segment. We focus only on new buildings and
+          high-level doskanalno know this area better than other specialists. Working directly with
+          developers - so have more useful information about the profitable deals.
+        </p>
+        <p className="about__text_footer">
+          We will never be late! <br /> Guaranteed secure transaction - 100%.
+        </p>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   return (
     <>
@@ -110,6 +138,7 @@ function App() {
         <Container>
           <Header />
           <Main />
+          <About />
         </Container>
       </Wrapper>
     </>
