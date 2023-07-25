fbfa31e13970a93ba7ed914b7d1c602fb7626f43
Yevhen <onufrii.socnet@gmail.com>
2023-07-25 23:13:38 +0300

    fix: burger styles

--- src/App.jsx
+++ src/App.jsx
@@ -13,46 +13,48 @@ function BurgerMenu() {
   }
   return (
     <>
-      <div onClick={e => onCheck(e)} className={checked ? 'burger burger-checked' : 'burger '}>
-        <div></div>
-      </div>
-      <div className={checked ? 'burger-menu burger-active' : 'burger-menu'}>
-        <nav className="menu">
-          <ul className="menu__list">
-            <li>
-              <a href="" className="menu__link">
-                Home
+      <div className="burger-box">
+        <div onClick={e => onCheck(e)} className={checked ? 'burger burger-checked' : 'burger '}>
+          <div></div>
+        </div>
+        <div className={checked ? 'burger-menu burger-active' : 'burger-menu'}>
+          <nav className="menu">
+            <ul className="menu__list">
+              <li>
+                <a href="" className="menu__link">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="" className="menu__link">
+                  About US
+                </a>
+              </li>
+              <li>
+                <a href="" className="menu__link">
+                  {' '}
+                  Gallery
+                </a>
+              </li>
+              <li>
+                <a href="" className="menu__link">
+                  Testimonials
+                </a>
+              </li>
+              <li>
+                <a href="" className="menu__link">
+                  Contacts
+                </a>
+              </li>
+            </ul>
+            <div className="menu__phone">
+              <a href="tel:+12345555555" className="menu__phone_number">
+                +1 234 555-55-55
               </a>
-            </li>
-            <li>
-              <a href="" className="menu__link">
-                About US
-              </a>
-            </li>
-            <li>
-              <a href="" className="menu__link">
-                {' '}
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href="" className="menu__link">
-                Testimonials
-              </a>
-            </li>
-            <li>
-              <a href="" className="menu__link">
-                Contacts
-              </a>
-            </li>
-          </ul>
-          <div className="menu__phone">
-            <a href="tel:+12345555555" className="menu__phone_number">
-              +1 234 555-55-55
-            </a>
-            <p className="menu__phone_text">call to order</p>
-          </div>
-        </nav>
+              <p className="menu__phone_text">call to order</p>
+            </div>
+          </nav>
+        </div>
       </div>
     </>
   )
